69faa6be3a7c0b9b78c17a79673381f0e3bc5463
amirashrafizham <amirashrafizham@gmail.com>
2024-04-06 03:02:21 +0800

    added nodes for data_engineering pipeline

--- orchestration/src/orchestration/pipelines/data_engineering/pipeline.py
+++ orchestration/src/orchestration/pipelines/data_engineering/pipeline.py
@@ -3,8 +3,24 @@ This is a boilerplate pipeline 'data_engineering'
 generated using Kedro 0.19.3
 """
 
-from kedro.pipeline import Pipeline, pipeline
+from kedro.pipeline import Pipeline, pipeline, node
+from .nodes import convert_to_datetime, convert_to_float
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline(
+        [
+            node(
+                func=convert_to_datetime,
+                inputs="mining_data",
+                outputs="df_converted_to_datetime",
+                name="convert_date_to_datetime_node",
+            ),
+            node(
+                func=convert_to_float,
+                inputs="df_converted_to_datetime",
+                outputs="df_converted_to_float",
+                name="convert_to_float",
+            ),
+        ]
+    )
